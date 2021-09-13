5679eb682656ddd8c84c88d3cdece7a20ffd8634
GrVasili <gritsuk09@gmail.com>
2021-09-13 22:44:42 +0300

    Initial Commit

---
+++ SwiftUIBasicElements1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIBasicElements1
+//
+//  Created by Vasili on 13.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

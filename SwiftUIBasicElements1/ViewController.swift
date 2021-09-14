62c1f48812694cbf8b6e5aaa2cb726d3dda14860
GrVasili <gritsuk09@gmail.com>
2021-09-14 21:07:57 +0300

    Add first task
    Make first task with button and label

--- SwiftUIBasicElements1/ViewController.swift
+++ SwiftUIBasicElements1/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var TF: UITextField!
+    @IBOutlet weak var Button: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Button.setTitle("Нажмите для регистрации", for: .normal)
+        Button.setTitle("Регистрация завершена", for: .disabled)
+       
     }
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        Label.text = ("Добро пожаловать, \(TF.text ?? "")!")
+        TF.text = ""
+        Button.isEnabled = false
+    
+    }
+    
 
 }
 
